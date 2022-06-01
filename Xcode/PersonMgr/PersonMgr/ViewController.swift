666c6a618464ab823737365b5a6eeab63b5d1174
eros404 <69208964+eros404@users.noreply.github.com>
2022-06-01 13:01:12 +0200

    PersonMgr

---
+++ Xcode/PersonMgr/PersonMgr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonMgr
+//
+//  Created by Utilisateur invité on 01/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
